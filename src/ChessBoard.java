3f5dc0930ef5865db36585fe8136e21f40c59712
Vincent Druesne <vt.druesne@gmail.com>
2020-02-11 15:34:28 +0100

    corrected name

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,7 +1,7 @@
 public class ChessBoard {
 
     public ChessBoard(){
-        ChessPieces board = new ChessPieces[8][8];
+        ChessPiece board = new ChessPiece[8][8];
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++){
 
