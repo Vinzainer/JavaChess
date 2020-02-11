0d1380f2406c44589d01e66649bb63e69d2cc47a
Vincent Druesne <vt.druesne@gmail.com>
2020-02-11 15:33:48 +0100

    change ChessPieces

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,7 +1,7 @@
 public class ChessBoard {
 
     public ChessBoard(){
-        Pawn board = new Pawn[8][8];
+        ChessPieces board = new ChessPieces[8][8];
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++){
 
