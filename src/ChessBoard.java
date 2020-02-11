d9b491f6de481f389e1d60c965d0d47333cf5069
WaIkn <frichetpierre98@gmail.com>
2020-02-11 15:26:29 +0100

    + ChessBoard, BoardCell & Main

---
+++ src/ChessBoard.java
@@ -0,0 +1,12 @@
+public class ChessBoard {
+
+    public ChessBoard(){
+        Pawn board = new Pawn[8][8];
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+
+            }
+        }
+    }
+
+}
