16b0d5e40f94a037dd414ce1653cda99fdb0cab6
WaIkn <frichetpierre98@gmail.com>
2020-02-13 11:35:03 +0100

    ~ modified constructor for positions

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -11,28 +11,28 @@ public class ChessBoard {
         
         for (int i = 0; i < 8; i++){
             if (i == 0 || i == 7){
-                board[0][i] = new Rook("Black");
-                board[7][i] = new Rook("White");
+                board[0][i] = new Rook("Black",0,i);
+                board[7][i] = new Rook("White",7,i);
             }
             else if (i == 1 || i == 6){
-                board[0][i] = new Knight("Black");
-                board[7][i] = new Knight("White");
+                board[0][i] = new Knight("Black",0,i);
+                board[7][i] = new Knight("White",7,i);
             }
             else if (i == 2 || i == 5){
-                board[0][i] = new Bishop("Black");
-                board[7][i] = new Bishop("White");
+                board[0][i] = new Bishop("Black",0,i);
+                board[7][i] = new Bishop("White",7,i);
             }
             else if (i == 3){
-                board[0][i] = new Queen("Black");
-                board[7][i] = new Queen("White");
+                board[0][i] = new Queen("Black",0,i);
+                board[7][i] = new Queen("White",7,i);
             }
             else if (i == 4){
-                board[0][i] = new King("Black");
-                board[7][i] = new King("White");
+                board[0][i] = new King("Black",0,i);
+                board[7][i] = new King("White",7,i);
             }
 
-            board[1][i] = new Pawn("Black");
-            board[6][i] = new Pawn("White");
+            board[1][i] = new Pawn("Black",1,i);
+            board[6][i] = new Pawn("White",6,i);
 
         }
 
@@ -80,13 +80,15 @@ public class ChessBoard {
 
     public int movePiece(int pieceX, int pieceY, int posx, int posy){
         /* int * int * int * int -> void */
-        if(posx >= 8 || posx < 0 || posy >= 8 || posy < 0) return 0;
-        if(pieceX >= 8 || pieceX < 0 || pieceY >= 8 || pieceY < 0) return 0;
-        ChessPiece tmpPiece = board[pieceX][pieceY];
-        if(tmpPiece == null) return 0;
-        
+        if(posx >= 8 || posx < 0 || posy >= 8 || posy < 0) return 0;            //out of range
+        if(pieceX >= 8 || pieceX < 0 || pieceY >= 8 || pieceY < 0) return 0;    //out of range
+        ChessPiece tmpPiece = board[pieceX][pieceY];                            
+        if(tmpPiece == null) return 0;                                          //no piece at board[pieceX][pieceY]
+        if(tmpPiece.getColor() == board[posx][posy].getColor()) return 0;       //piece of same color
+
         board[posx][posy] = tmpPiece;
         board[pieceX][pieceY] = null;
+        tmpPiece.setPosition(posx,posy);
 
         return 1;
     }
