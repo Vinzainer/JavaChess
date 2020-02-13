a9876356ed6aff926e582660bda685672cf5e9bf
WaIkn <frichetpierre98@gmail.com>
2020-02-13 10:25:02 +0100

    ~ modified toString return to be more visual

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -11,27 +11,27 @@ public class ChessBoard {
         
         for (int i = 0; i < 8; i++){
             if (i == 0 || i == 7){
-                board[0][i] = new Rook("black");
+                board[0][i] = new Rook("Black");
                 board[7][i] = new Rook("White");
             }
             else if (i == 1 || i == 6){
-                board[0][i] = new Knight("black");
+                board[0][i] = new Knight("Black");
                 board[7][i] = new Knight("White");
             }
             else if (i == 2 || i == 5){
-                board[0][i] = new Bishop("black");
+                board[0][i] = new Bishop("Black");
                 board[7][i] = new Bishop("White");
             }
             else if (i == 3){
-                board[0][i] = new Queen("black");
+                board[0][i] = new Queen("Black");
                 board[7][i] = new Queen("White");
             }
             else if (i == 4){
-                board[0][i] = new King("black");
+                board[0][i] = new King("Black");
                 board[7][i] = new King("White");
             }
 
-            board[1][i] = new Pawn("black");
+            board[1][i] = new Pawn("Black");
             board[6][i] = new Pawn("White");
 
         }
@@ -42,13 +42,13 @@ public class ChessBoard {
         /* this -> String */
         String str = "";
         for (int i = 0; i < 8; i++){
-            str += "\n";
+            str += "\n|";
             for (int j = 0; j < 8; j++){
                 if (board[i][j] == null){
-                    str += "null ";
+                    str += " + |";
                 }
                 else {
-                    str += board[i][j].toString() + " ";
+                    str += board[i][j].toString().substring(0,2) + board[i][j].getColor().charAt(0) + "|";
                 }
             }
         }
